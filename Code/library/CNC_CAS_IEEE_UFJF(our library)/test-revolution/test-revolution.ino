431e8035cb71608cbc4a19ce3ef856569d776409
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2019-03-31 15:42:24 -0300

    our library

---
+++ Code/library/CNC_CAS_IEEE_UFJF(our library)/test-revolution/test-revolution.ino
@@ -0,0 +1,25 @@
+#include "CNC_CAS_IEEE_UFJF.h"
+
+const int stepsPerRevolution = 200; //1.8º por passo
+Stepper_CNC myStepper(stepsPerRevolution, 16, 5, 4, 0);
+//                                        D0,D1,D2,D3
+
+
+void setup ()
+{
+  myStepper.setSpeed(60);
+
+  Serial.begin(115200);
+}
+
+void loop ()
+{
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(1000);
+}
